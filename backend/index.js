41aa76861e5bfb386dc0ede2cfb5ee29c61e52ac
anupam-m4 <anupvergil99@gmail.com>
2025-11-10 20:07:52 +0530

    Final fix: Removed payment router reference from index.js

--- backend/index.js
+++ backend/index.js
@@ -20,8 +20,8 @@ connectDB();
 // middlewares
 app.use(express.json());
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-// app.use(cors({ origin: "https://grocery-frontend-eta.vercel.app", credentials: true }));
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: "https://grocery-frontend-eta.vercel.app", credentials: true }));
+// app.use(cors({ origin: "*", credentials: true }));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
